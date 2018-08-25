3ed3fc45f518eeaead072ac80e0b3c5a7102e342
student <student@itstep1.dp.ua>
2018-08-25 10:46:14 +0300

    Add project files.

---
+++ ConsoleApplication4/Vocabulary.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Vocabulary.h"
+
+
+Vocabulary::Vocabulary()
+{
+}
+
+
+Vocabulary::~Vocabulary()
+{
+}
---
+++ ConsoleApplication4/Vocabulary.h
@@ -0,0 +1,8 @@
+#pragma once
+class Vocabulary
+{
+public:
+	Vocabulary();
+	~Vocabulary();
+};
+
